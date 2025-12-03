e58e3b72f256a1dd8cd9ca92853e49abd00e75c7
nikitavydyshev <101637275+nikitavydyshev@users.noreply.github.com>
2025-12-03 16:55:15 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+# ==========================
+# CLICK SETTINGS
+# ==========================
+CLICK_SERVICE_ID = "87456"
+CLICK_MERCHANT_ID = "50415"
+CLICK_SECRET_KEY = "beQQrHvozzH"
+CLICK_MERCHANT_USER_ID = "69637"   # если не нужен — оставь ""
+
+
+# ==========================
+# BOTHELP SETTINGS
+# ==========================
+BOTHELP_ID = "574704:e1a97fcf5deeffe1ea80909096a6178d"
+BOTHELP_SECRET = "574704:39fdd14df52f1e5eed9d983d310f1cc4"
+
+
+# ==========================
+# GOOGLE DRIVE LINK
+# ==========================
+GOOGLE_FILE_URL = "https://drive.google.com/drive/folders/1XJNg1A8fJCjkUOyoDobndS9HumV023yb"
+
+
+# ==========================
+# GENERAL SETTINGS
+# ==========================
+DOMAIN = "https://vasybot.onrender.com"   # пример: https://mybot.onrender.com
+CODE_WORD = "гайд"                # кодовое слово, можно изменить
+PAY_AMOUNT = 1000               # сумма платежа в сумах
