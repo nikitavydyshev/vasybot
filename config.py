ba50f31b816d1e43c1e75f8366ff091bf1b0b51a
nikitavydyshev <101637275+nikitavydyshev@users.noreply.github.com>
2025-12-05 15:16:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -6,23 +6,5 @@ CLICK_MERCHANT_ID = "50415"
 CLICK_SECRET_KEY = "beQQrHvozzH"
 CLICK_MERCHANT_USER_ID = "69637"   # если не нужен — оставь ""
 
+CLICK_BASE_URL = "https://api.click.uz"
 
-# ==========================
-# BOTHELP SETTINGS
-# ==========================
-BOTHELP_ID = "574704:e1a97fcf5deeffe1ea80909096a6178d"
-BOTHELP_SECRET = "574704:39fdd14df52f1e5eed9d983d310f1cc4"
-
-
-# ==========================
-# GOOGLE DRIVE LINK
-# ==========================
-GOOGLE_FILE_URL = "https://drive.google.com/drive/folders/1XJNg1A8fJCjkUOyoDobndS9HumV023yb"
-
-
-# ==========================
-# GENERAL SETTINGS
-# ==========================
-DOMAIN = "https://vasybot.onrender.com"   # пример: https://mybot.onrender.com
-CODE_WORD = "гайд"                # кодовое слово, можно изменить
-PAY_AMOUNT = 1000               # сумма платежа в сумах
